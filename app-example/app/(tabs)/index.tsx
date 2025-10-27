8c93dd9baf3f9e9616dc7e0a4827569540c725ed
Wanjohi Wanjiru <wanjiruwanjohi@gmail.com>
2025-10-27 10:15:50 +0300

    added app-example

---
+++ app-example/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+export default function IndexScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Welcome to my first Expo app!</Text>
+    </View>
+  );
+}
+
